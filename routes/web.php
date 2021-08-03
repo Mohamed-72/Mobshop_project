e224897f74cbc945ab3140bedaa5eec8e02c0ab3
Mohamed Hamdy <Mohamed.hamdy4@yahoo.com>
2021-08-03 14:01:22 +0200

    add the assets folder in public and create shop file and cart file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
 });
